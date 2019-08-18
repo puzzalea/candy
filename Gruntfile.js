083511b882ef5c83bef98552759b03d66b325f4d
Cara Michele <caraheacock@gmail.com>
2019-08-18 16:55:03 -0500

    Updates dev dependencies

--- Gruntfile.js
+++ Gruntfile.js
@@ -45,5 +45,5 @@ module.exports = function(grunt) {
 	grunt.loadNpmTasks('grunt-contrib-concat');
 	grunt.loadNpmTasks('grunt-contrib-sass');
 	grunt.loadNpmTasks('grunt-contrib-watch');
-	grunt.registerTask('default',['watch']);
+	grunt.registerTask('default', ['sass', 'concat', 'usebanner:js']);
 };
